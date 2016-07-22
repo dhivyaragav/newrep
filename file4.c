f50c6269cfc889554c99a090c159f14a5a79f018
dhivyaragav <dhivyashanthi10@gmail.com>
2016-07-22 12:23:03 +0530

    file4.c

---
+++ file4.c
@@ -0,0 +1,18 @@
+main()
+{
+int a,b,c;
+printf("\n Enter the numbers");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b)&&(a>c)
+{
+printf("A is bigger");
+}
+elseif(b>c)&&(b>a)
+{
+printf("B is bigger");
+}
+else
+{
+printf("C is bigger");
+}
+}
