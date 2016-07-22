0ab162062ff8662105fb9ed973c46711d13455c9
dhivyaragav <dhivyashanthi10@gmail.com>
2016-07-22 12:17:04 +0530

    file3.c

---
+++ file3.c
@@ -0,0 +1,13 @@
+main()
+{
+char c;
+printf("\n Enter the character");
+scanf("%c",c);
+if(c=='a'||'e'||'i'||'o'||'u')
+{
+printf("\n The character is vowel");
+}
+else
+{
+printf("\n The character is constant");
+}
